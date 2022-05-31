a310b3f82e99c44dbfbd5cd5d2833b404a61e1a0
francivaldolima <francivaldolimadesousa@gmail.com>
2022-05-31 16:41:36 -0300

    Modulos

---
+++ modulos/atividade-prática/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade,mostraCidade,mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Naruto",20));
+console.log(mostraCidade("Naruto","Aldeia da Folha"));
+console.log(mostraHobby("Naruto","Comer Lámen"));
